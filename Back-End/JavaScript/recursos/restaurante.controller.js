f873dd4e61eb0498db3b4cd271dbf468d59b63fc
Danilo Duarte <43526477+danzinduarte@users.noreply.github.com>
2020-03-14 00:28:49 -0300

    crud restaurante finalizado

--- Back-End/JavaScript/recursos/restaurante.controller.js
+++ Back-End/JavaScript/recursos/restaurante.controller.js
@@ -49,7 +49,6 @@ function salvaRestaurante(req,res){
 		res.status(404).json({
 			sucesso: false, 
 			msg: "Formato de entrada inválido.",
-			erro : req.body
 		})
 		return;
     }	
@@ -77,55 +76,79 @@ function salvaRestaurante(req,res){
 }
 function excluiRestaurante(req,res)
 {
-	if (!req.params.id) 
-	{
-		res.status(409).json(
-		{
+	if (!req.params.id) {
+		return res.status(400).json({
 			sucesso: false,
 			msg: "Formato de entrada inválido."
-		})
-		return;
+		});
 	}
-	dataContext.conexao.transaction(function(t) 
-	{
-		let restaurante
-		dataContext.Restaurante.findByPk(req.params.id, {transaction : t})
-		.then(function(restauranteEncontrado)
-		{
-			if (!restauranteEncontrado) 
-			{
-				res.status(404).json(
-				{
-					sucesso: false,
-					msg: "Restaurante não encontrado."
-				})
-				return;
-			}
-			
-			return dataContext.Restaurante.findByPk(id, {transaction : t})
+
+	// Quando tu for trabalhar com apenas um model e que ele nao vai fazer outras insercoes em outras tabelas, vc nao precisa utilizar transacao
+	dataContext.Restaurante.findByPk(req.params.id).then( function(restaurante) {
+		
+		if (!restaurante) {
+			return res.status(404).json({
+				sucesso: false,
+				msg: "Restaurante não encontrado."
+			})
+		}
+
+		//restaurante = restaurante.get({ plain : true })
+		dataContext.Restaurante.destroy({ where : { id : req.params.id }}).then(function(result) {
+			return res.status(200).json({
+				sucesso: true,
+				msg: 'Restaurante excluido com sucesso!'
+			})
 		})
-		.then(function(restauranteRetornado) 
-		{
-			restauranteRetornado.destroy({transaction : t})
-		})
-	})
-	.then(function()
-	{
-		res.status(200).json(
-		{
-        	sucesso:true,
-        	msg: "Registro excluído com sucesso",
-        	data: []
-        })	        	
-	})
-	.catch(function(erro)
-	{
-		res.status(409).json(
-		{ 
+
+	}).catch(function(error){
+		return res.status(400).json({ 
 			sucesso: false,
-			msg: "Falha ao excluir o restaurante" 
+			msg: "Falha ao excluir o restaurante",
+			erro: error 
 		});	
 	})
+	
+	// dataContext.conexao.transaction(function(t) 
+	// {
+	// 	let restaurante
+	// 	dataContext.Restaurante.findByPk(req.params.id, {transaction : t})
+	// 	.then(function(restauranteEncontrado)
+	// 	{
+	// 		if (!restauranteEncontrado) 
+	// 		{
+	// 			res.status(404).json(
+	// 			{
+	// 				sucesso: false,
+	// 				msg: "Restaurante não encontrado."
+	// 			})
+	// 			return;
+	// 		}
+			
+	// 		return dataContext.Restaurante.findByPk(id, {transaction : t})
+	// 	})
+	// 	.then(function(restauranteRetornado) 
+	// 	{
+	// 		restauranteRetornado.destroy({transaction : t})
+	// 	})
+	// })
+	// .then(function()
+	// {
+	// 	res.status(200).json(
+	// 	{
+    //     	sucesso:true,
+    //     	msg: "Registro excluído com sucesso",
+    //     	data: []
+    //     })	        	
+	// })
+	// .catch(function(erro)
+	// {
+	// 	res.status(409).json(
+	// 	{ 
+	// 		sucesso: false,
+	// 		msg: "Falha ao excluir o restaurante" 
+	// 	});	
+	// })
 }
 
 function atualizaRestaurante(req,res){
@@ -134,53 +157,88 @@ function atualizaRestaurante(req,res){
 	let restaurante	= req.body
 
 	if (!restaurante) {
-		res.status(404).json({
+		return res.status(400).json({
 			sucesso: false,
 			msg: "Formato de entrada inválido.",
 			erro : req.body
-		})
-		return;
+		})		
 	}
 
-	//Inicia transaction
-	dataContext.conexao.transaction(function(t) {
-
-		//Pesquise antes de atualizar
-		dataContext.Restaurante.findByPk(req.params.id, {transaction : t})
-		
-		.then(function(restaurante){
-			if (!restaurante) {
-				res.status(404).json({
-					sucesso: false,
-					msg: "Restaurante não encontrado.",
-					erro : restaurante
-				})
-				return;
-			}
-			
-			//Campos da restaurante que serão alterados
-			let updateFields = {
-				nomeDoRestaurante			: restaurante.nomeDoRestaurante
-			}
-
-			//Atualiza somente os campos restaurante
-			restaurante.update(updateFields, {transaction : t})
+	if(!req.params.id) {
+		return res.status(400).json({
+			sucesso: false,
+			msg: "Um id deve ser informado!",
 		})
-	}).then(function(restauranteAtualizado) {	
-		res.status(200).json({
-        sucesso:true,
-        msg: "Registro atualizado com sucesso",
-        data: restauranteAtualizado
-        	})	
-	})
-	//Roolback
-	.catch(function(erro){
-		console.log(erro);
-		res.status(409).json({ 
+	}
+
+	dataContext.Restaurante.findByPk(req.params.id).then(function(restauranteBanco){
+		if (!restauranteBanco) {
+			return res.status(404).json({
+				sucesso: false,
+				msg: "Restaurante não encontrado."				
+			});
+		}
+
+		// Campos da restaurante que serão alterados
+		let updateFields = {
+			nomeDoRestaurante			: restaurante.nomeDoRestaurante
+		}
+
+		// Atualiza somente os campos restaurante
+		restauranteBanco.update(updateFields).then(function(restauranteAtualizado){
+			return res.status(200).json({
+				sucesso:true,
+				msg: "Registro atualizado com sucesso",
+				data: restauranteAtualizado
+			})	
+		})
+
+	}).catch(function(error){
+		return res.status(400).json({ 
 			sucesso: false,
 			msg: "Falha ao atualizar o restaurante" 
 		});	
 	})
+
+	// //Inicia transaction
+	// dataContext.conexao.transaction(function(t) {
+
+	// 	//Pesquise antes de atualizar
+	// 	dataContext.Restaurante.findByPk(req.params.id, {transaction : t})
+		
+	// 	.then(function(restaurante){
+	// 		if (!restaurante) {
+	// 			res.status(404).json({
+	// 				sucesso: false,
+	// 				msg: "Restaurante não encontrado.",
+	// 				erro : restaurante
+	// 			})
+	// 			return;
+	// 		}
+			
+	// 		//Campos da restaurante que serão alterados
+	// 		let updateFields = {
+	// 			nomeDoRestaurante			: restaurante.nomeDoRestaurante
+	// 		}
+
+	// 		//Atualiza somente os campos restaurante
+	// 		restaurante.update(updateFields, {transaction : t})
+	// 	})
+	// }).then(function(restauranteAtualizado) {	
+	// 	res.status(200).json({
+    //     sucesso:true,
+    //     msg: "Registro atualizado com sucesso",
+    //     data: restauranteAtualizado
+    //     	})	
+	// })
+	// //Roolback
+	// .catch(function(erro){
+	// 	console.log(erro);
+	// 	res.status(409).json({ 
+	// 		sucesso: false,
+	// 		msg: "Falha ao atualizar o restaurante" 
+	// 	});	
+	// })
 }
 
 module.exports = 
